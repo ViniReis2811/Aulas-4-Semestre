2102d681620c52874b026964461e540c0d7cfdaf
ViniReis2811 <vinireis2811@gmail.com>
2026-03-18 20:45:08 -0300

    segundo commit do biscoito da sorte

--- Desenvolvimento Web/projeto_base_js/jest.config.ts
+++ Desenvolvimento Web/projeto_base_js/jest.config.ts
@@ -6,4 +6,13 @@ export default {
     restoreMocks: true,
     collectCoverageFrom: ['src/**/*.ts'],
     coverageDirectory: 'coverage',
+    collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            statements: 90,
+            branches: 90,
+            functions: 90,
+            lines: 90
+        }
+    }
 };
